d3d7386635bc05c0faeb7b56f8658516d9bb88f6
techTutorial <talk.to.amit.verma@gmail.com>
2020-09-12 10:29:14 +0530

    updates > working

--- src/test/java/es/example/sb/ng/ApplicationTests.java
+++ src/test/java/es/example/sb/ng/ApplicationTests.java
@@ -1,12 +1,14 @@
 package es.example.sb.ng;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.Test; //Junit4
+//import org.junit.jupiter.api.Test; //Junit5
+import org.junit.runner.RunWith; //Junit4
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringBootCommandLineRunnerApp.class)
 public class ApplicationTests {
 
 	@Test
